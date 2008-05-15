d0b683099e2c527d396f23c3245996c740fc7506
Joseph Javier Perla <not_email @AT@@ jperla.com>
2008-05-14 18:44:14 -0700

    saving changes

--- generator.py
+++ generator.py
@@ -1,36 +1,38 @@
 import os
 import sys
 import commands
+import glob
 
-def change_v1_to_v2(v1):
-    """
-    >>> v1 = 'This is version 1.0 of the game'
-    >>> print change_v1_to_v2(v1)
-    This is version 2.0 of the game
-    """
-    v2 = v1.replace('1.0','2.0')
-    return v2
+import utilities
 
-def generate_2(v1_filename):
-    assert(os.path.exists('%s' % v1_filename))
-
+def change_v1_to_v2(v1_filename):
     v1 = open(v1_filename, 'r').read()
+    v2 = v1.replace('1.0','2.0')
+    f = open(v1_filename, 'w')
+    f.write(v2)
+    f.close()
 
-    v2 = change_v1_to_v2(v1)
-    v2_filename = 'url/' + v1_filename+'_2'
+def generate_2(v1_directory):
+    assert(os.path.exists('%s' % v1_directory))
 
-    v2_file = open(v2_filename, 'w')
-    v2_file.write(v2)
-    v2_file.close()
+    v2_directory = 'game-2.0'
 
-    print commands.getoutput('tar cf %s.tar %s' % (v2_filename, v2_filename))
-    print commands.getoutput('chmod 777 %s.tar' % v2_filename)
-    print commands.getoutput('sudo mv %s.tar /var/www/' % v2_filename)
-    print commands.getoutput('rm %s' % v2_filename)
+    print commands.getoutput('cp -rf %s %s' % (v1_directory, v2_directory))
+    assert(os.path.exists('%s' % v2_directory))
 
-    assert(os.path.exists('%s' % v1_filename))
+    assert(os.path.exists('%s/game.py' % v2_directory))
+    change_v1_to_v2('%s/game.py' % v2_directory)
+    
+    print commands.getoutput('tar cf %s.tar %s' % (v2_directory, v2_directory))
+    assert(os.path.exists('%s.tar' % v2_directory))
 
-generate_2('game.py')
+    print commands.getoutput('gzip %s.tar' % v2_directory)
+    assert(os.path.exists('%s.tar.gz' % v2_directory))
+
+    print commands.getoutput('mv %s.tar.gz /var/www/' % v2_directory)
+    assert(os.path.exists('/var/www/%s.tar.gz' % v2_directory))
+
+generate_2('game-1.0')
 
 def _test():
     import doctest
